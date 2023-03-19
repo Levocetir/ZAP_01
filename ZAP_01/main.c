87cdf32b5f4ba3dd22708d8339a9a374eaedba0f
kretp <kretp@LAPTOP-0VQFKJCG>
2023-03-19 13:36:46 +0100

    Přidejte soubory projektu.

---
+++ ZAP_01/main.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<math.h>
+
+void main() {
+
+	/*
+
+	//pr 1
+	printf("James Boond \\ \"Agent 007\" \\ # 150%% zaruka # /\nSpol. s rucenim omezenym\n");
+
+	//pr 2
+	printf("\007 CHYBA \n");
+
+	//pr 3
+	printf("Toto je 'backslash'  '\\'\n");
+
+	//pr 4
+	char i;          //deklarace
+	i = 0;           //definice
+
+	scanf("%c",&i);  //nacteni znaku v konzoli
+	i++;
+	printf("%c ASCII \n", i, i);
+
+
+	//pr 5
+	int cislo = 0;
+	printf("zadej cislo: \n");
+	scanf("%X", &cislo);
+	printf("%d\n", cislo);
+
+	//pr 6
+	double castka;
+	printf("Zadej bez DPH: \n");
+	scanf("%lf", &castka);
+
+	printf("Castka s DPH je: %f \n", castka * 1.21);
+
+	//pr 7 TEST!
+	float a;
+	float b;
+	float c;
+
+	printf("Zadejte stranu a:\n");
+	scanf("%f", &a);
+
+	printf("Zadejte stranu b:\n");
+	scanf("%f", &b);
+
+	printf("Zadejte stranu c:\n");
+	scanf("%f", &c);
+
+	float povrch = 2 * (a * b + b * c + a * c);
+
+	printf("Povrch kvádru je: \n");
+	printf("%f", povrch);
+
+	//pr 8
+	int a = 4, b = 8, c = -2, d = 0, e = 3;
+	printf("%d \n", a++ / ++c * --e);
+	printf("%d \n", --b * c++ - a);
+	printf("%d \n", -b - --c);
+	printf("%d \n", ++a - --e);
+	printf("%d \n", e / --a * b++ / c++);
+	printf("%d \n", a %= b = d = 1 + e / 2);
+
+	*/
+	//pr 9
+
+	int i = 8, j = 3, k;
+	printf("%d \n", i++);
+	printf("%d \n", j += i);
+	printf("%d \n", k = 12 + j);
+	printf("%d \n", j = ++i);
+	printf("%d \n", j = i++);
+	printf("%d \n", k = --j + 2);
+
+
+}
